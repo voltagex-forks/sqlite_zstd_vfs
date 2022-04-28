f22c04d804b99995009cd6b80405e5f175e06e62
Adam Baxter <git@voltagex.org>
2022-04-28 20:59:34 +1000

    Builds on Windows, but at a cost, see below
    * *Removes* web_vfs functionality, although I only ifdef'd out the
    registration of the extension
    
    * Hacks up CMakeLists to use vcpkg on Windows
    
    * Uses upstream sqlitecpp to deal with weird CMake+vcpkg issues
    
    * Does not build tests
    
    * Adds ugly #ifdef __declspec(dllexport) syntax to the methods we need
    to export for sqlite3.exe to load the DLLs on Windows
    There is probably a better syntax for this
    
    * Undocumented, untested
    
    * Sorry

--- src/nested_vfs.cc
+++ src/nested_vfs.cc
@@ -4,22 +4,33 @@
 #include <sqlite3ext.h>
 SQLITE_EXTENSION_INIT1
 #include "SQLiteNestedVFS.h"
-#include "web_vfs.h"
 
+#ifdef HAS_WEB
+#include "web_vfs.h"
+#endif
 /*************************************************************************************************/
 
 /*
 ** This routine is called when the extension is loaded.
 ** Register the new VFS.
 */
+#ifdef _WIN32
+extern "C" __declspec(dllexport) int sqlite3_nestedvfs_init(sqlite3 *db, char **pzErrMsg,
+                                      const sqlite3_api_routines *pApi) {
+#else
 extern "C" int sqlite3_nestedvfs_init(sqlite3 *db, char **pzErrMsg,
                                       const sqlite3_api_routines *pApi) {
+#endif
     int rc = SQLITE_OK;
     SQLITE_EXTENSION_INIT2(pApi);
+
+    #ifdef HAS_WEB
     rc = (new WebVFS::VFS())->Register("web");
     if (rc != SQLITE_OK) {
         return rc;
     }
+    #endif
+    
     rc = (new SQLiteNested::VFS())->Register("nested");
     if (rc == SQLITE_OK) {
         rc = SQLITE_OK_LOAD_PERMANENTLY;
--- src/zstd_vfs.cc
+++ src/zstd_vfs.cc
@@ -3,7 +3,11 @@
 */
 #include <sqlite3ext.h>
 SQLITE_EXTENSION_INIT1
+
+#ifdef HAS_WEB
 #include "web_vfs.h"
+#endif
+
 #include "zstd_vfs.h"
 
 /*************************************************************************************************/
@@ -12,14 +16,24 @@ SQLITE_EXTENSION_INIT1
 ** This routine is called when the extension is loaded.
 ** Register the new VFS.
 */
+
+#ifdef _WIN32
+extern "C" __declspec(dllexport) int sqlite3_zstdvfs_init(sqlite3 *db, char **pzErrMsg,
+                                    const sqlite3_api_routines *pApi) {
+#else
 extern "C" int sqlite3_zstdvfs_init(sqlite3 *db, char **pzErrMsg,
                                     const sqlite3_api_routines *pApi) {
+#endif
     int rc = SQLITE_OK;
     SQLITE_EXTENSION_INIT2(pApi);
+    
+    #ifdef HAS_WEB
     rc = (new WebVFS::VFS())->Register("web");
     if (rc != SQLITE_OK) {
         return rc;
     }
+    #endif
+    
     rc = (new ZstdVFS())->Register("zstd");
     if (rc == SQLITE_OK) {
         rc = SQLITE_OK_LOAD_PERMANENTLY;
